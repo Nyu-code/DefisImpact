14aa50ca3220f5b99a241d2d2b7de11503efbb4c
Souuum <73708286+Souuum@users.noreply.github.com>
2020-10-30 17:44:27 +0100

    Add files via upload

---
+++ SousODD.java
@@ -0,0 +1,28 @@
+public class SousODD{
+    private String rep;
+	private int id;
+	
+	public SousODD(String r, int i) {
+		this.setRep(r);
+		this.setId(i);
+	}
+
+	public String getRep() {
+		return rep;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setRep(String rep) {
+		this.rep = rep;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
+
+
+}
