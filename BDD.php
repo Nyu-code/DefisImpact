704eba1e47eaf7326478b66acd78c65e6e9579e8
matthewjoy-prog <73708506+matthewjoy-prog@users.noreply.github.com>
2020-11-29 02:06:46 +0100

    Add files via upload

--- BDD.php
+++ BDD.php
@@ -58,23 +58,76 @@ class BDD {
 
 		$inds["ind_sdg"] = pg_fetch_all($req_sdg);
 
-		$req_sdg_compass = pg_query_params($this->bd, 'SELECT * FROM ind_sdg_compass WHERE sdg = $1 and target = $2', array($odd, $cat));
+		$req_sdg_compass = pg_query_params($this->bd, 'SELECT * FROM ind_sdg_compass WHERE sdg = $1 and indicator_field = $2', array($odd, $cat));
 
 		$inds["ind_sdg_compass"] = pg_fetch_all($req_sdg_compass);
 
-		$req_cerise_et_iris = pg_query_params($this->bd, 'SELECT * FROM ind_cerise_et_iris WHERE sdg = $1 and target = $2', array($odd, $cat));
+		$req_cerise_et_iris = pg_query_params($this->bd, 'SELECT * FROM ind_cerise_et_iris WHERE sdg = $1 and indicator_field = $2', array($odd, $cat));
 
 		$inds["ind_cerise_et_iris"] = pg_fetch_all($req_cerise_et_iris);
 
-		$req_iris = pg_query_params($this->bd, 'SELECT * FROM ind_iris WHERE sdg = $1 and target = $2', array($odd, $cat));
+		$req_iris = pg_query_params($this->bd, 'SELECT * FROM ind_iris WHERE sdg = $1 and indicator_field = $2', array($odd, $cat));
 
 		$inds["ind_iris"] = pg_fetch_all($req_iris);
 
-		$req_cerise = pg_query_params($this->bd, 'SELECT * FROM ind_cerise WHERE sdg = $1 and target = $2', array($odd, $cat));
+		$req_cerise = pg_query_params($this->bd, 'SELECT * FROM ind_cerise WHERE sdg = $1 and indicator_field = $2', array($odd, $cat));
 
 		$inds["ind_rise"] = pg_fetch_all($req_cerise);
 
 		return $inds;
+	}
+
+	public function get_inds_par_mot_cles($mots_cles, $tab = null){
+		$res = [];
+		if ($tab == null){
+			$inds = [];
+			$req_sdg = pg_query($this->bd, 'SELECT * FROM ind_sdg');
+
+			$inds["ind_sdg"] = pg_fetch_all($req_sdg);
+
+			// $req_sdg_compass = pg_query($this->bd, 'SELECT * FROM ind_sdg_compass');
+
+			// $inds["ind_sdg_compass"] = pg_fetch_all($req_sdg_compass);
+
+			// $req_cerise_et_iris = pg_query($this->bd, 'SELECT * FROM ind_cerise_et_iris');
+
+			// $inds["ind_cerise_et_iris"] = pg_fetch_all($req_cerise_et_iris);
+
+			// $req_iris = pg_query($this->bd, 'SELECT * FROM ind_iris');
+
+			// $inds["ind_iris"] = pg_fetch_all($req_iris);
+
+			// $req_cerise = pg_query($this->bd, 'SELECT * FROM ind_cerise');
+
+			// $inds["ind_rise"] = pg_fetch_all($req_cerise);
+
+			foreach ($inds as $c => $v){
+				$res[$c] = []; 
+				foreach ($v as $c2 => $v2){
+					foreach ($v2 as $c3 => $v3) {
+						foreach ($mots_cles as $k => $mot) {
+							if (preg_match("/\b$mot\b/i", $v3)){
+								array_push($res[$c], $v2);
+							}
+						}
+					}
+				}
+			}
+		} 
+		else {
+			foreach ($tab as $c => $v){
+				foreach ($v as $c2 => $v2){
+					foreach ($v2 as $c3 => $v3) {
+						foreach ($mots_cles as $k => $mot) {
+							if (preg_match("/\b$mot\b/i", $v3)){
+								array_push($res[$c], $v2);
+							}
+						}
+					}
+				}
+			}
+		}
+		return $res;
 	}
 
 }
